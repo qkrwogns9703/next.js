cf9f40d1090ae3c40ce8deb1dffed404ff2c633f
Jiachi Liu <inbox@huozhi.im>
2024-07-04 00:38:52 +0200

    refactor

--- packages/next/src/client/components/react-dev-overlay/internal/helpers/use-error-handler.ts
+++ packages/next/src/client/components/react-dev-overlay/internal/helpers/use-error-handler.ts
@@ -23,67 +23,64 @@ const rejectionQueue: Array<Error> = []
 const errorHandlers: Array<ErrorHandler> = []
 const rejectionHandlers: Array<ErrorHandler> = []
 
-if (typeof window !== 'undefined') {
-  function handleError(error: unknown) {
-    if (
-      !error ||
-      !(error instanceof Error) ||
-      typeof error.stack !== 'string'
-    ) {
-      // A non-error was thrown, we don't have anything to show. :-(
-      return
-    }
+function handleError(error: unknown) {
+  if (!error || !(error instanceof Error) || typeof error.stack !== 'string') {
+    // A non-error was thrown, we don't have anything to show. :-(
+    return
+  }
 
-    const isCausedByHydrationFailure = isHydrationError(error)
-    if (
-      isHydrationError(error) &&
-      !error.message.includes(
-        'https://nextjs.org/docs/messages/react-hydration-error'
-      )
-    ) {
-      const reactHydrationDiffSegments = getReactHydrationDiffSegments(
-        error.message
-      )
-      let parsedHydrationErrorState: typeof hydrationErrorState = {}
-      if (reactHydrationDiffSegments) {
+  const isCausedByHydrationFailure = isHydrationError(error)
+  if (
+    isHydrationError(error) &&
+    !error.message.includes(
+      'https://nextjs.org/docs/messages/react-hydration-error'
+    )
+  ) {
+    const reactHydrationDiffSegments = getReactHydrationDiffSegments(
+      error.message
+    )
+    let parsedHydrationErrorState: typeof hydrationErrorState = {}
+    if (reactHydrationDiffSegments) {
+      parsedHydrationErrorState = {
+        ...(error as any).details,
+        ...hydrationErrorState,
+        warning: hydrationErrorState.warning || [
+          getDefaultHydrationErrorMessage(),
+        ],
+        notes: reactHydrationDiffSegments[0],
+        reactOutputComponentDiff: reactHydrationDiffSegments[1],
+      }
+    } else {
+      // If there's any extra information in the error message to display,
+      // append it to the error message details property
+      if (hydrationErrorState.warning) {
+        // The patched console.error found hydration errors logged by React
+        // Append the logged warning to the error message
         parsedHydrationErrorState = {
           ...(error as any).details,
+          // It contains the warning, component stack, server and client tag names
           ...hydrationErrorState,
-          warning: hydrationErrorState.warning || [
-            getDefaultHydrationErrorMessage(),
-          ],
-          notes: reactHydrationDiffSegments[0],
-          reactOutputComponentDiff: reactHydrationDiffSegments[1],
         }
-      } else {
-        // If there's any extra information in the error message to display,
-        // append it to the error message details property
-        if (hydrationErrorState.warning) {
-          // The patched console.error found hydration errors logged by React
-          // Append the logged warning to the error message
-          parsedHydrationErrorState = {
-            ...(error as any).details,
-            // It contains the warning, component stack, server and client tag names
-            ...hydrationErrorState,
-          }
-        }
-        error.message +=
-          '\nSee more info here: https://nextjs.org/docs/messages/react-hydration-error'
       }
-      ;(error as any).details = parsedHydrationErrorState
+      error.message +=
+        '\nSee more info here: https://nextjs.org/docs/messages/react-hydration-error'
     }
-
-    // Only queue one hydration every time
-    if (isCausedByHydrationFailure) {
-      if (!hasHydrationError) {
-        errorQueue.push(error)
-      }
-      hasHydrationError = true
-    }
-    for (const handler of errorHandlers) {
-      handler(error)
-    }
+    ;(error as any).details = parsedHydrationErrorState
   }
+
+  // Only queue one hydration every time
+  if (isCausedByHydrationFailure) {
+    if (!hasHydrationError) {
+      errorQueue.push(error)
+    }
+    hasHydrationError = true
+  }
+  for (const handler of errorHandlers) {
+    handler(error)
+  }
+}
+
+if (typeof window !== 'undefined') {
   // These event handlers must be added outside of the hook because there is no
   // guarantee that the hook will be alive in a mounted component in time to
   // when the errors occur.
